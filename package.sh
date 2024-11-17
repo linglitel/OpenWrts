a906928162c5abff506ac9902ff93564bd1fbbee
linglitel <90138887+linglitel@users.noreply.github.com>
2024-11-17 16:45:07 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -7,9 +7,6 @@ git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherap
 # luci-theme-neobird
 git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 
-# Mentohust
-git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
-
 # UnblockNeteaseMusic
 git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
 
